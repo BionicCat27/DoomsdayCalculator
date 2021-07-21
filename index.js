d546eb3c2d9179623a6ffea9b6be40c293299d73
Aslanbb27 <aslan.bennington.barr@gmail.com>
2021-07-22 01:02:19 +1200

    Basic additional logic and tidying

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ var DateNum = document.getElementById("DateNum");
 
 var DoomsdayResult = document.getElementById("DoomsdayResult");
 
+//Doomsday 0001 = wednesday
+var Doomsday0001 = 3;
+
 function calculateDoomsday() {
+
+    //Get input values
+    var YearNumValue = YearNum.value;
+    var MonthNumValue = MonthNum.value;
+    var DateNumvalue = DateNum.value;
+
     //Anchor calculation
     //Year calculation
     //Month calulation
