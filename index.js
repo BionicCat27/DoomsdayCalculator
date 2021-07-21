9a08da9376ab52aba2c5aac813201dd86452c2e8
Aslanbb27 <aslan.bennington.barr@gmail.com>
2021-07-22 00:53:14 +1200

    Basic JS setup

---
+++ index.js
@@ -0,0 +1,17 @@
+var DoomsdayButton = document.getElementById("DoomsdayButton");
+DoomsdayButton.addEventListener("click", function() {
+    calculateDoomsday();
+});
+
+var YearNum = document.getElementById("YearNum");
+var MonthNum = document.getElementById("MonthNum");
+var DateNum = document.getElementById("DateNum");
+
+var DoomsdayResult = document.getElementById("DoomsdayResult");
+
+function calculateDoomsday() {
+    //Anchor calculation
+    //Year calculation
+    //Month calulation
+    //Date calculation
+}
